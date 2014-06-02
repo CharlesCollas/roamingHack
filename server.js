ff2a4916445a98f5c2f5c2abaea247a7bb40dbce
Shubham SHARMA <shubham@sharma.fr>
2014-06-02 19:43:21 +0100

    imporve for sending

--- server.js
+++ server.js
@@ -10,9 +10,23 @@ server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
-server.post('/lol', function (req, res, next) {
-	console.log(req.params);
+server.post('/takeoff', function (req, res, next) {
+		console.log(req.params);
+
+		var from = "";
+		var to = "";
+		var text = "";
+
+		var res = from + "\n";
+		var res += to + "\n";
+		var res += text + "\n";
+
+		var exec = require('child_process').exec;
+		exec("./send.js 0613508170 " + res , puts);
+
+
     res.send(200);
+
     return next();
 });
 
