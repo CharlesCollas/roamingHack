dcc0f934264d27b881ae5a520b13d0139a52d11f
Charles Collas <collas.charles@gmail.com>
2014-06-03 00:21:53 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
-// le gestion des routes
 var restify = require('restify');
-var exec = require('child_process').exec;
+// var exec = require('child_process').exec;
 
 var server = restify.createServer({
 	name: 'MyServer',
@@ -12,31 +11,29 @@ server.use(restify.queryParser());
 server.use(restify.bodyParser());
 
 server.post('/takeoff', function (req, res, next) {
-		console.log(req.params);
+	console.log(req.params);
+	
+	var from = "";
+	var to = "";
+	var text = "";
+	
+	var result = from + "\n" + to + "\n" + text + "\n";
+	
+	function puts(error, stdout, stderr) { sys.puts(stdout); }
+	
+	var command = "/usr/bin/gammu --sendsms TEXT 0613508170 -len " + result.length + " -text \"" + result + "\"";
+	console.log(command);
 
-		var from = "";
-		var to = "";
-		var text = "";
-
-		var result = from + "\n" + to + "\n" + text + "\n";
-
-		function puts(error, stdout, stderr) { sys.puts(stdout); }
-
-		var command = "/usr/bin/gammu --sendsms TEXT 0613508170 -len " + result.length + " -text \"" + result + "\"";
-
-		console.log(command);
-
-    res.send(200);
-
-    return next();
+    	res.send(200);
+    	return next();
 });
 
 server.get('/', function (req, res, next) {
 	console.log('GET /');
-    res.send(200);
-    return next();
+    	res.send(200);
+    	return next();
 });
 
 server.listen(4242, function() {
-  console.log('%s started listening',  server.name);
+  	console.log('%s started listening',  server.name);
 });
