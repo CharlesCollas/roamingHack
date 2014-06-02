040f5a3ba690fcda60aea47e495b2cb8bea9c0bb
Charles Collas <collas.charles@gmail.com>
2014-06-03 00:44:09 +0200

    include node modules + exec gammu

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var restify = require('restify');
-// var exec = require('child_process').exec;
+var sys = require('sys');
+var exec = require('child_process').exec;
 
 var server = restify.createServer({
 	name: 'MyServer',
 	version: '1.0.0'
 });
+
+function puts(error, stdout, stderr) { sys.puts(stdout); }
 
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
@@ -22,16 +25,18 @@ server.post('/takeoff', function (req, res, next) {
 	function puts(error, stdout, stderr) { sys.puts(stdout); }
 	
 	var command = "/usr/bin/gammu --sendsms TEXT 0613508170 -len " + result.length + " -text \"" + result + "\"";
+	
 	console.log(command);
+	exec(command, puts);
 
-    	res.send(200);
-    	return next();
+    res.send(200);
+    return next();
 });
 
 server.get('/', function (req, res, next) {
 	console.log('GET /');
-    	res.send(200);
-    	return next();
+    res.send(200);
+    return next();
 });
 
 server.listen(4242, function() {
